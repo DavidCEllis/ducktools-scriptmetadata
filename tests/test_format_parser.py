15437aca585c65a08b6cf674c47c586e67fe4625
David C Ellis <ducksual@gmail.com>
2023-10-21 19:44:40 +0100

    More tests

--- tests/test_format_parser.py
+++ tests/test_format_parser.py
@@ -1,12 +1,20 @@
 from ducktools.pep723parser import PEP723Parser
 from pathlib import Path
+from packaging.specifiers import SpecifierSet
+from packaging.requirements import Requirement
 
 example_folder = Path(__file__).parent / "example_files"
 
 pep_723_ex_raw = """
-
+# /// pyproject
+# [run]
+# requires-python = ">=3.11"
+# dependencies = [
+#   "requests<3",
+#   "rich",
+# ]
+# ///
 """
-
 
 pep_723_ex_extracted = """
 [run]
@@ -17,11 +25,62 @@ dependencies = [
 ]
 """.strip()
 
+pep_723_ex_extracted_dict = {
+    "run": {
+        "requires-python": ">=3.11",
+        "dependencies": [
+            "requests<3",
+            "rich",
+        ]
+    }
+}
 
-def test_pep_example_parse_text():
+pep_723_script_dependencies = {
+    "requires-python": SpecifierSet(">=3.11"),
+    "dependencies": [
+        Requirement("requests<3"),
+        Requirement("rich"),
+    ]
+}
+
+
+def test_pep_example_file_raw():
     test_file = example_folder / "pep-723-sample.py"
 
     parser = PEP723Parser.from_path(test_file)
-    output = parser.get_first_raw_toml("pyproject").strip()
-
+    output = parser.get_pyproject_raw().strip()
     assert output == pep_723_ex_extracted
+
+
+def test_pep_example_file_toml():
+    test_file = example_folder / "pep-723-sample.py"
+    parser = PEP723Parser.from_path(test_file)
+    output = parser.get_pyproject_toml()
+
+    assert output == pep_723_ex_extracted_dict
+
+
+def test_pep_example_file_script_dependencies():
+    test_file = example_folder / "pep-723-sample.py"
+    parser = PEP723Parser.from_path(test_file)
+    output = parser.script_dependencies
+
+    assert output == pep_723_script_dependencies
+
+
+def test_pep_example_text_raw():
+    parser = PEP723Parser.from_string(pep_723_ex_raw)
+    output = parser.get_pyproject_raw().strip()
+    assert output == pep_723_ex_extracted
+
+
+def test_pep_example_text_toml():
+    parser = PEP723Parser.from_string(pep_723_ex_raw)
+    output = parser.get_pyproject_toml()
+    assert output == pep_723_ex_extracted_dict
+
+
+def test_pep_example_text_script_dependencies():
+    parser = PEP723Parser.from_string(pep_723_ex_raw)
+    output = parser.script_dependencies
+    assert output == pep_723_script_dependencies
