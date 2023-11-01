1e492350b1c664a373035f790c114744647629e3
David C Ellis <ducksual@gmail.com>
2023-11-01 19:20:31 +0000

    Update perf files to use new class methods

--- perf/import_parse_toml.py
+++ perf/import_parse_toml.py
@@ -1,9 +1,9 @@
 import os.path
 
-from ducktools.pep723parser import metadata_from_path
+from ducktools.pep723parser import EmbeddedMetadata
 
 pth = os.path.realpath(f"{os.path.dirname(__file__)}/../examples/pep-723-sample.py")
 
-data = metadata_from_path(pth)
+data = EmbeddedMetadata.from_path(pth)
 
 output = data.run_requirements_text
