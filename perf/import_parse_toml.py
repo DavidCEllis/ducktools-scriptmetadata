729346b6d39d8e7f1d705069f4bef745e4c965cf
David C Ellis <ducksual@gmail.com>
2023-11-01 18:50:17 +0000

    Update perf examples

--- perf/import_parse_toml.py
+++ perf/import_parse_toml.py
@@ -1,9 +1,9 @@
 import os.path
 
-from ducktools.pep723parser import EmbeddedMetadataParser
+from ducktools.pep723parser import metadata_from_path
 
 pth = os.path.realpath(f"{os.path.dirname(__file__)}/../examples/pep-723-sample.py")
 
-data = EmbeddedMetadataParser.from_path(pth)
+data = metadata_from_path(pth)
 
-output = data.plain_script_dependencies
+output = data.run_requirements_text
